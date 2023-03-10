27ec0d0961fad697e3690a8a1e4a73f8c5cf1b8f
SERRES Sabrina <Sabrina.Serres@bordeaux-inp.fr>
2023-03-10 17:02:27 +0100

    socket d'écoute

--- Gossip.py
+++ Gossip.py
@@ -2,23 +2,33 @@
 import multiprocessing as mp
 import ipaddress
 import random
+import socket
 
 ## Fonctions de protocol Gossip
 
 
-def Node(ID,IP,V):
+def Node(ID,IP,V,nodes):
 
     print(f"Je suis le noeud d'ID {ID} et d'IP : {IP} ")
-    
-    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, 0))  # bind to a random port on the node's IP address
+    server_socket.listen()
+
+    port = server_socket.getsockname()
+    nodes[ID] = (IP, port)
+    print(f"Node {ID} listening on {IP}:{port}")
+
+
 def Create_Nodes(N):
     ID=100000
+    nodes = {}
     for i in range(0,N):
         IP_1 = random.choice(range(0,255))
         IP_2 = random.choice(range(0,255))
-        IP = f'192.168.{IP_1}.{IP_2}'
+        #IP = f'192.168.{IP_1}.{IP_2}'
+        IP = f'127.0.0.1'
         ID+=1
-        Process=mp.Process(target=Node,args=(ID,IP,[],))
+        Process=mp.Process(target=Node,args=(ID,IP,[],nodes))
         
         
         Process.start()
