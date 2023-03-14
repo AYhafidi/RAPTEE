c689e5d2ad9ada467e48ef8165523aa9a66adf00
Aymane Hafidi <ahafidi@bordeaux-inp.fr>
2023-03-14 17:53:14 +0100

    Version Pyro4

--- main.py
+++ main.py
@@ -3,20 +3,5 @@
 import Raptee
 import Gossip
 import Brahms
-
-n=100
-ID_list=[]
-
-
-IP=Gossip.IP_Array(n)
-
-
-#Gossip.Create_Nodes(100)
-if __name__ == '__main__':
-    Gossip.Create_Nodes(n,IP)
-
-
-
-
-
+import Pyro4
 
