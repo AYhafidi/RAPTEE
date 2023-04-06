91cf5517dd9517d0466f2b962af398e8539d6fec
HAFIDI Aymane <Aymane.Hafidi@bordeaux-inp.fr>
2023-04-06 17:24:28 +0200

    Script lancé c, nodes sont connectés

--- Orchest.py
+++ Orchest.py
@@ -1,11 +1,93 @@
-
 import os
 import sys
 import csv
 import socket
 import subprocess
 import pickle
+import time
+import select
+                                                   # # #  POLL FUNCTION  # # # 
 
+def Poll_function(sockets, Machines_Names):
+
+    # Initialisation
+    pollerObject = select.poll()
+
+    # Adding sockets
+    for i in sockets:
+        pollerObject.register(i, select.POLLIN)
+
+    # Traiter les données
+    while(True):
+
+        fdVsEvent = pollerObject.poll(-1)
+
+        for descriptor, Event in fdVsEvent:
+            Index=sockets.index(descriptor)
+            if Index%2==0 and Event & select.POLLIN:
+                Out=os.read(descriptor,2048).decode()
+                print(f"[ {Machines_Names[Index//2]} ] Out : {Out}",end="")
+
+            elif Index%2==1 and Event & select.POLLIN:
+                Err=os.read(descriptor,2048).decode()
+                print(f"[ {Machines_Names[Index//2]} ] Err : {Err}",end="")
+
+
+                                                    # # # <---  Functions ---> # # #
+
+def Gossip_connect(IP_addr,Port):
+    conn_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn_socket.connect((IP_addr, int(Port)))
+    return conn_socket
+
+
+def send_data(socket,data):
+    socket.send(pickle.dumps(data))
+
+def recv_data(socket,Size):
+    return pickle.loads(socket.recv(Size))
+
+                                                # # # Function d'initialisation # # #
+def Net_init():
+    
+    # Peer connection mapping
+    peer_conn={}
+
+    # se connecter avec le processus pére
+    conn_socket=Gossip_connect(sys.argv[1],sys.argv[2])
+
+    # Créer une socket d'écoute
+    Hostname=socket.gethostname() #Nom de la machine
+    clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.bind((Hostname,0))
+    Port=clientsocket.getsockname()[1]
+
+    # Envoyer les infos
+    data=[Hostname,Port]
+    send_data(conn_socket,data)
+
+    # Recv Nmbr de processus + rang + Mapping des machines
+    Nmbr_procs, proc_id, machine_dict=recv_data(conn_socket,1024)
+
+    # Lancer l'écoute
+    clientsocket.listen(Nmbr_procs-1)
+
+    # accepter les connexions des machines de rang supérieur
+    for i in range(proc_id,Nmbr_procs-1):
+        sock, addr = clientsocket.accept()
+        rang = recv_data(sock,1024)
+        peer_conn[rang]=sock
+
+    # se connecter avec les machines de rang inférieur
+    for i in range(0,proc_id):
+        peer_conn[i]=Gossip_connect(machine_dict[i][0],machine_dict[i][1])
+        send_data(peer_conn[i],proc_id)
+    sys.stdout.flush()
+    sys.stderr.flush()
+    
+
+
+                                                ############## MAIN ###############
 
 def main():
     # Verifier les arguments
@@ -17,6 +99,11 @@ def main():
 
     # Mapping des infos
     machine_dict={}
+
+    # Mapping des sockets
+    sockets={}
+    # Fd des tubes
+    fd_Pipes=[]
 
     # Machine names
     with open(sys.argv[1],'r') as file:
@@ -34,15 +121,40 @@ def main():
 
     # Lancer les procs
     for Name in machine_Names:
-        #List des arguments
+        #Liste des arguments
+        OutpipeR ,OutpipeW=os.pipe()
+        ErrpipeR ,ErrpipeW=os.pipe()
         Args=["ssh",Name,"python3","~/Desktop/RAPTEE/"+Executable,Hostname,str(Port)]+sys.argv[3:]
-        subprocess.run(Args, timeout=None)
+        pid=os.fork()    
+        if pid==0:
+            # Redirection des tubes
+            os.close(OutpipeR)
+            os.dup2(OutpipeW, sys.stdout.fileno())
+            os.close(ErrpipeR)
+            os.dup2(ErrpipeW, sys.stderr.fileno())
+            os.close(OutpipeW)
+            os.close(ErrpipeW)
+            os.execvp(Args[0],Args)
+        elif pid>0:
+            os.close(OutpipeW)
+            os.close(ErrpipeW)
+            fd_Pipes.append(OutpipeR)
+            fd_Pipes.append(ErrpipeR)
 
     # Accepter les connexions
     for i in range(Nmbr_procs):
         sock_accept,addr=serversocket.accept()
-        machine_dict[i]=pickle.loads(sock_accept.recv(1024))
-    print(machine_dict)
+        machine_dict[i]=recv_data(sock_accept,1024)
+        sockets[i]=sock_accept
+
+    # Envoi Nombre de processus + rang + Mapping des machines
+    for i in range(Nmbr_procs):
+        send_data(sockets[i],[Nmbr_procs,i,machine_dict])
+    
+    #fonction Poll
+    Poll_function(fd_Pipes, machine_Names)
+
+
 
 
 
--- hello.py
+++ hello.py
@@ -1,19 +1,134 @@
-
+#!/usr/bin/env python3
+import random
 import socket
+import time
+import ipaddress
+from Orchest import *
 import sys
-import pickle
-
-Hostname=socket.gethostname()
-clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.bind((Hostname,0))
-clientsocket.listen(2)
-Port=clientsocket.getsockname()[1]
 
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((sys.argv[1], int(sys.argv[2])))
+id_base = 10000
+max_storage=1
+n=5
 
-# Envoyer les infos
-data=[Hostname,Port]
-s.send(pickle.dumps(data))
+class Node:
+    def __init__(self, max_storage, id,ip):
+       
+        # Node info 
+        self.max_storage = max_storage  # maximum storage capacity of node
+        self.id = id   # Node's ID
+        self.ip = ip   # Node's IP address
+        self.Nu = random.sample(range(id_base,id_base+n),max_storage)  # neighbor list
+        self.Su = []  # sample list
+       
+        # Interconnections' info
+        self.neighbor_sockets_and_info = {}  # Neighbors' IPs and ports 
+       
+        # Listening socket and binding 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Listening socket
+        self.sock.bind((str(self.ip), 0))  # bind socket to node id
+        self.port=self.sock.getsockname()[1] 
+        self.sock.listen(max_storage)  # listen for incoming connections
+        #print("Node", self.id, "listening on port", self.port)
+    
+    def get_Nui(self, i):
+        if i < len(self.Nu):
+            return self.Nu[i]
+        else:
+            return None
+
+    def get_Sui(self, i):
+        if i < len(self.Su):
+            return self.Su[i]
+        else:
+            return None
+        
+
+    def update_neighbor_list(self, Nu_t):
+        # This method updates the sample list of the node with the first max_storage elements of the list Su
+        self.Nu = Nu_t[:self.max_storage]  # update neighbor list with the first max_storage elements
+ 
+    def update_sample_list(self, Su_t):
+        # This method updates the sample list of the node with the first max_storage elements of the list Su
+        self.Su = Su_t[:self.max_storage]  # update sample list with the first max_storage elements
+ 
+   
+
+
+# Sampling class
+
+class Sampler:
+    def __init__(self, size):
+        self.h = self.minwise_hash(size)
+        self.min_value = float('inf')
+        self.values = set()
+        
+    def next(self, value):
+        hash_value = self.h(value)
+        if hash_value < self.min_value:
+            self.min_value = hash_value
+            self.values = set([value])
+        elif hash_value == self.min_value:
+            self.values.add(value)
+            
+    def sample(self):
+        return random.sample(self.values, 1)[0]
+    
+    def minwise_hash(self, size):
+        a = random.randint(1, size - 1)
+        b = random.randint(0, size - 1)
+        return lambda x: (a * x + b) % size
+                                                                ##### Initialisation des connexions #####
+Net_init()
+
+
+
+
+def sending_and_receiving(node, num_neighbour, data):
+    send_data(node.neighbor_sockets_and_info[node.Nu[num_neighbour]][1],data)
+    print(f"Node :{node.id - id_base} sent his ID to his {num_neighbour+1} neighbor")
+    string=recv_data(node.neighbor_sockets_and_info[node.Nu[num_neighbour]][2],1024)
+    print('Node ' + str(node.Nu[num_neighbour]-id_base) + ' received : ' + str(string))
+    sys.stdout.flush()
+    sys.stderr.flush()
+     
+
+# Creating and initiaizing nodes
+
+
+
+
+# Nodes
+base_ip = ipaddress.IPv4Address('127.0.0.1')
+nodes=[Node(n, id_base+i, base_ip+i) for i in range(n)]
+
+
+for i in range(0, n):
+    # print ('\n  NEW NODE :')
+    for j in range (0,max_storage):
+        neighbor_id=nodes[i].Nu[j]
+        neighbor_ip = nodes[neighbor_id-10000].ip   # For the initial gossip connection, the nodes know their neighbors' connexion info. This won't be the case outside of this loop
+        neighbour_port = nodes[nodes[i].Nu[j]-10000].port
+        neighbor_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket for neighbor j connection  
+        # print('Neighbour ' + str(j+1) + ' is ' + str(nodes[i].Nu[j] - 10000))
+        neighbor_sock.connect((str(neighbor_ip), neighbour_port )) # node i connecting to neighbor j
+        conn, address = nodes[nodes[i].Nu[j] - 10000].sock.accept()   # neighbour j accepting the connection 
+        Port=conn.getsockname()[1]
+        nodes[i].neighbor_sockets_and_info[neighbor_id]=[[neighbor_ip,neighbour_port], neighbor_sock, conn]   # adding neighbor's info connection : his [IP,port],  
+        
+    
+
+# # Testing by sending and receiving IDs (push)
+
+# print('\n TESTING EXCHANGES :')
+
+
+sending_and_receiving(nodes[1], 0, nodes[1].id)
+# nodes[80].neighbor_sockets[1].send(str(nodes[80].id).encode())
+# print('Node ' + str(80) + ' sent his ID to his 1st neighbor ')
+# string = nodes[80].personal_neighbor_sockets[1].recv(1024).decode()
+# print('Node ' + str(nodes[80].Nu[1]-id_base) + ' received : ' + str(string))
+
+while(1):
+    continue
